1ce103f40860ddd427760d69a7ab02349d6d9841
millket <milliyon.ketera@gmail.com>
2020-06-03 22:30:56 -0500

    menu

--- orders/models.py
+++ orders/models.py
@@ -36,3 +36,7 @@ class Sub(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.small} -{self.large}"
+
+class Pasta(models.Model):
+    name=models.CharField(max_length=64)
+    price=models.DecimalField(max_digits=4,decimal_places=2)
