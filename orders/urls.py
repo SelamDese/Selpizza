bc1b0e6caff9311709b1179408b767a07e60a17a
millket <milliyon.ketera@gmail.com>
2020-07-06 21:27:58 -0500

    working

--- orders/urls.py
+++ orders/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
-    path("menu/<str:category>", views.menu, name="menu"),
+    path("", views.index, name="index"),
+    path("menu/<str:category>", views.menu, name="menu")
 ]
