b280a4f620eacdf00dbfe6daad1c436d95affaa6
millket <milliyon.ketera@gmail.com>
2020-07-06 21:28:31 -0500

    working

--- orders/views.py
+++ orders/views.py
@@ -8,3 +8,20 @@ def index(request):
         "orders" : Order.object.all()
     }
     return render(request, "orders/index.html", context)
+
+def menu(request,category):
+    menu,columns=findTable(category)
+    order_number=User_order.objects.get(user=request.user,status='initiated').order_number
+    context = {
+        "user":request.user,
+        "Checkout":Order2.objects.filter(user=request.user,number=order_number),
+        "Checkout_category":Order2.objects.filter(user=request.user,number=order_number).values_list('category').distinct(),
+        "Total":list(Order2.objects.filter(user=request.user,number=order_number).aggregate(Sum('price')).values())[0],
+        "Category": Category.objects.all(),
+        "Active_category":category,
+        "Menu": menu,
+        "Columns":columns,
+        "Topping_price": 0.00,
+        "Order_number":order_number
+    }
+    return render(request,"menu.html",context)
