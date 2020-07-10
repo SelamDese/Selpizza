2a5d2ff5e8729f85a1dc40f52c3216e609f39888
millket <milliyon.ketera@gmail.com>
2020-07-09 22:50:45 -0500

    working views.py

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Category,Regular_pizza,Sicilian_pizza,Topping,Sub,Pasta,Salad,Dinner_platter
 
 # Create your views here.
+def index(request):
+    # return HttpResponse("hello")
+    context = {
+        "Category": Category.objects.all()
+    }
+    return render(request,"index.html",context)
